05709ea3d685b9adc59122dcb04e98daea999813
Chi <rectaflex@gmail.com>
2022-02-17 16:15:03 -0500

    1

--- ms_hm/setup.py
+++ ms_hm/setup.py
@@ -6,9 +6,6 @@ setup(ext_modules=cythonize([
     Extension(
         "utils",
         ["cython/utils.pyx"],
-        extra_compile_args=["-O3", "-fopenmp", "-std=c++14"],
-        extra_link_args=["-fopenmp"],
-        language="c++",
     )
 ],
                             language_level=3))
