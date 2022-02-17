dbfe710b802ccb5d0df1e0cd9fd90e0739a262b0
Chi <rectaflex@gmail.com>
2022-02-17 15:10:18 -0500

    packaging stuff

---
+++ ms_hm/MS.py
@@ -0,0 +1,307 @@
+import sys
+import numpy as np
+import scipy.integrate as integ
+import scipy.interpolate as interp
+
+class MS:
+
+    def __init__(self, R, m, U, w, alpha, A, rho0, deltaL, L, trace_ray=False):
+        self.R = R
+        self.m = m
+        self.U = U
+        self.w = w
+        self.deltaL = deltaL
+        self.L = L
+        self.A = A
+        self.alpha = alpha
+        self.N = R.shape[0]
+        self.exec_pos = -1
+
+
+        self.t0 = self.alpha * np.sqrt(3 / (8*np.pi*rho0))
+        self.t = t0
+        self.xi = 0
+
+        self.RH = self.t0 / self.alpha
+        self.Abar = self.A / self.RH
+        self.Abar_stg = self.to_stg(self.Abar)
+
+        self.q = 1
+        self.deltau_i = self.cfl_deltau(self.R, self.m, self.U) * 0.05
+        self.deltau_adap = self.deltau_i
+
+        # initialize the poton
+        self.trace_ray = trace_ray
+        self.Abar_p = self.Abar[0]
+        self.U_p = self.U[0]
+        self.m_p = self.m[0]
+        self.R_p = self.R[0]
+
+        self.U_hm = np.zeros(self.N-1)
+        self.m_hm = np.zeros(self.N-1)
+        self.R_hm = np.zeros(self.N-1)
+        self.xi_hm = np.zeros(self.N-1)
+
+        self.U_hm[0] = self.U[0]
+        self.m_hm[0] = self.m[0]
+        self.R_hm[0] = self.R[0]
+
+        self.xi_hm[0] = 0
+        self.r_old = 0
+        return
+    # convert to half grid
+    def to_stg(self,arr):
+        return (arr[0:-1] + arr[1:]) / 2
+    def to_cubic_stg(self,arr):
+        a1 = arr[0:-1] ** 3
+        a2 = arr[1:] ** 3
+        return (a1 + a2) / (np.abs(a1 + a2)) * ( np.abs(a1 + a2)/ 2) **(1/3)
+    def to_idx(self, pos):
+        return np.searchsorted(self.Abar, pos, "right") - 1
+    def gamma(self, R, m, U, xi):
+        return np.sqrt(np.exp(2 * (1 - self.alpha) * xi)
+                       + (self.Abar * R)**2 * (U**2 - m))
+    def P(self, rho) :
+        return self.w * rho
+    def rho(self, R, m):
+        return m + ms_rho_term(R, m, self.Abar)
+
+    def psi(self, rho, p, Pprime):
+        #return np.log(rho ** (-3 * self.alpha * self.w / 2))
+        offset = + np.log(rho[-1]**(-3 * self.alpha * self.w / 2))
+        return inv_derv_phi(rho, p, offset)
+
+    def Pprime(self, R, m):
+        R_stg = self.to_stg(R)
+        m_stg = self.to_stg(m)
+        rho_stg = m_stg + ms_rho_term_stg(R, m, self.Abar, R_stg, self.Abar_stg)
+        return self.w * np.concatenate( ([0], stg_dfdA(rho_stg, self.Abar_stg) ,[0] ))
+
+        mprime = dfdA(m, self.deltaL, self.Abar, 0, self.exec_pos)
+        Rprime = dfdA(R, self.deltaL, self.Abar, 0, self.exec_pos)
+        mpp = ddfddA(m, self.deltaL, self.Abar, 0, self.exec_pos)
+        Rpp = ddfddA(R, self.deltaL, self.Abar, 0, self.exec_pos)
+        return self.w * (4 * self.Abar**2 * mprime * Rprime**2
+                + R**2 * (4 * mprime + self.Abar * mpp)
+               + self.Abar * R * (self.Abar * Rprime * mpp + mprime * (6 * Rprime - self.Abar * Rpp))) \
+                / (3 * (R + self.Abar * Rprime)**2)
+    def k_coeffs(self, R, m, U, Abar_p, xi) :
+        g = self.gamma(R, m, U, xi)
+        r = self.rho(R, m)
+        p = self.P(r)
+
+        Pprime = self.Pprime(R, m)
+        ep = np.exp(self.psi(r, p, Pprime))
+        #print(g,r,p,ep)
+        kR = self.alpha * R * (U * ep - 1)
+        km = 2 * m - 3 * self.alpha * U * ep * (p + m)
+
+        AR_prime = R + self.Abar * dfdA(R, self.deltaL, self.Abar, 0, self.exec_pos)
+
+        kU = U - self.alpha * ep * \
+            (   g**2 * np.concatenate( ([self.w
+                            * ((m[1] + m[1] - 2 * m[0]) /( self.Abar[1]**2)) *  (1**2 * 5/3)] ,Pprime[1:] /
+             (self.Abar[1:]) ))  / (R * (AR_prime) * (r + p))
+            + (2 * U**2 + m + 3 * p) / 2)
+
+        #kA_p = self.alpha * interp.griddata(self.Abar, ep * g / AR_prime,
+        #                                    Abar_p, method='cubic')
+        kA_p = self.alpha * np.interp(Abar_p,self.Abar, ep * g / AR_prime)
+        return kR, km, kU, kA_p
+
+
+
+    def run_steps(self,n_steps, exc_intv=0) :
+        step = 0
+
+        deltau = self.deltau_i
+        if(self.trace_ray == True):
+            print("Tracing ray is enabled and excision will be performed!")
+        else:
+            print('Not Tracing ray and NO excision will be performed!')
+
+        while(step < n_steps) :
+
+            if(self.to_idx(self.Abar_p) > 50 and self.to_idx(self.Abar_p) < self.N * 0.8):
+                self.exec_pos = np.max([self.exec_pos, self.to_idx(self.Abar_p) - 10])
+
+            exec_arr = np.concatenate(([0] * (self.exec_pos+1),[1] * (self.N - self.exec_pos - 1)))
+
+            kR1, km1, kU1, kA_p1 = self.k_coeffs(self.R, self.m, self.U, self.Abar_p, self.xi)
+            kR2, km2, kU2, kA_p2 = self.k_coeffs(self.R + deltau/2*kR1, self.m + deltau/2*km1,
+                                                 self.U + deltau/2*kU1, self.Abar_p + deltau/2*kA_p1,
+                                                self.xi + deltau / 2)
+            kR3, km3, kU3, kA_p3 = self.k_coeffs(self.R + deltau/2*kR2, self.m + deltau/2*km2,
+                                                 self.U + deltau/2*kU2, self.Abar_p + deltau/2*kA_p2,
+                                                self.xi + deltau/2)
+            kR4, km4, kU4, kA_p4 = self.k_coeffs(self.R + deltau*kR3, self.m + deltau*km3,
+                                                 self.U + deltau*kU3, self.Abar_p + deltau*kA_p3,
+                                                self.xi + deltau)
+
+            self.R = self.R + (deltau/6*(kR1 + 2*kR2 + 2*kR3 + kR4)) * exec_arr
+            self.m = self.m + (deltau/6*(km1 + 2*km2 + 2*km3 + km4)) * exec_arr
+            self.U = self.U + (deltau/6*(kU1 + 2*kU2 + 2*kU3 + kU4)) * exec_arr
+
+            if(self.trace_ray == True):
+                Abar_p_new = self.Abar_p + deltau/6*(kA_p1 + 2*kA_p2 + 2*kA_p3 + kA_p4)
+                idx_p_new = self.to_idx(Abar_p_new)
+                #U_p_new = interp.griddata(self.Abar, self.U, Abar_p_new, method='cubic')
+                #m_p_new = interp.griddata(self.Abar, self.m, Abar_p_new, method='cubic')
+                #R_p_new = interp.griddata(self.Abar, self.R, Abar_p_new, method='cubic')
+
+                U_p_new = np.interp(Abar_p_new, self.Abar, self.U)
+                m_p_new = np.interp(Abar_p_new, self.Abar, self.m)
+                R_p_new = np.interp(Abar_p_new, self.Abar, self.R)
+
+                diff = idx_p_new - self.to_idx(self.Abar_p)
+                if (diff > 1): ##move across more than two grid pints!
+                    print('Warning!' + str(self.Abar_p) + ' ' + str(Abar_p_new))
+                    #break
+                if ( diff > 0): # move across one grid point
+                    interp_w = (self.Abar[idx_p_new] - self.Abar_p) / (Abar_p_new - self.Abar_p)
+                    # linear interpolation
+                    self.U_hm[idx_p_new] = U_p_new * interp_w + self.U_p * (1 - interp_w)
+                    self.m_hm[idx_p_new] = m_p_new * interp_w + self.m_p * (1 - interp_w)
+                    self.R_hm[idx_p_new] = R_p_new * interp_w + self.R_p * (1 - interp_w)
+                    self.xi_hm[idx_p_new] = self.xi + deltau * interp_w
+
+                if(self.xi >= self.xi_hm[0]): #only start advancing photon when the system time is large enough
+                    self.Abar_p = Abar_p_new
+                    self.U_p = U_p_new
+                    self.m_p = m_p_new
+                    self.R_p = R_p_new
+
+                if(idx_p_new == self.N-2): #going out of the boundary
+                    break
+
+            step+=1
+            self.xi += deltau
+
+            if(step % 10 == 0):
+                if(find_exec_pos(self.R**2 * self.m * self.Abar**2 * np.exp(2 * (self.alpha-1) * self.xi)) > 0):
+                    break
+
+        if step < n_steps:
+            print("Photon has gone out of the outter boundary or horizon is identified!")
+
+
+    def adap_run_steps(self,n_steps, adjust_steps=100, tol=1e-7) :
+        step = 0
+        deltau = self.deltau_adap
+
+        if(self.trace_ray == True):
+            print("Tracing ray is enabled and excision will be performed!")
+        else:
+            print('Not Tracing ray and NO excision will be performed!')
+
+
+        while(step < n_steps):
+
+            if (deltau < 1e-9):
+                print("Warning, the time step is too small!")
+                break
+
+            if(self.to_idx(self.Abar_p) > 50 and self.to_idx(self.Abar_p) < self.N * 0.8):
+                self.exec_pos = np.max([self.exec_pos, self.to_idx(self.Abar_p) - 10])
+            exec_arr = np.concatenate(([0] * (self.exec_pos+1),[1] * (self.N - self.exec_pos - 1)))
+
+            kR1, km1, kU1, kA_p1 = self.k_coeffs(self.R, self.m, self.U, self.Abar_p, self.xi)
+            kR2, km2, kU2, kA_p2 = self.k_coeffs(self.R + deltau/2*kR1, self.m + deltau/2*km1,
+                                                 self.U + deltau/2*kU1, self.Abar_p + deltau/2*kA_p1,
+                                                 self.xi + deltau/2)
+
+            kR3, km3, kU3, kA_p3 = self.k_coeffs(self.R + 3/4*deltau*kR2, self.m + 3/4*deltau*km2,
+                                                 self.U + 3/4*deltau*kU2, self.Abar_p + 3/4*deltau*kA_p2,
+                                                self.xi + 3/4*deltau)
+
+
+            Abar_p_new = self.Abar_p + deltau/9*(2*kA_p1 + 3*kA_p2 + 4*kA_p3 )
+            R_new = self.R + deltau/9*(2*kR1 + 3*kR2 + 4*kR3 )
+            m_new = self.m + deltau/9*(2*km1 + 3*km2 + 4*km3 )
+            U_new = self.U + deltau/9*(2*kU1 + 3*kU2 + 4*kU3 )
+
+            # calculating the difference of the photon position
+            # diff is always zero when tracing_ray is false
+            idx_p_new = self.to_idx(Abar_p_new)
+            diff = idx_p_new - self.to_idx(self.Abar_p)
+
+            kR4, km4, kU4, kA_p4 = self.k_coeffs(R_new , m_new , U_new, Abar_p_new, self.xi + deltau)
+
+            E_R = np.max( np.abs((deltau * (-5*kR1/72 + kR2/12 + kR3/9 - kR4/8))) * exec_arr)
+            E_m = np.max( np.abs(deltau * (-5*km1/72 + km2/12 + km3/9 - km4/8)) * exec_arr)
+            E_U = np.max( np.abs(deltau * (-5*kU1/72 + kU2/12 + kU3/9 - kU4/8)) * exec_arr)
+
+            max_err_R = np.max(np.abs(self.R) * exec_arr) * tol
+            max_err_m = np.max(np.abs(self.m) * exec_arr) * tol
+            max_err_U = np.max(np.abs(self.U) * exec_arr) * tol
+
+            if(diff <= 1 and E_R < max_err_R and E_m < max_err_m and E_U < max_err_U):
+
+                self.R = self.R + deltau/9*(2*kR1 + 3*kR2 + 4*kR3 ) * exec_arr
+                self.m = self.m + deltau/9*(2*km1 + 3*km2 + 4*km3 ) * exec_arr
+                self.U = self.U + deltau/9*(2*kU1 + 3*kU2 + 4*kU3 ) * exec_arr
+
+                if(self.trace_ray == True):
+
+                    U_p_new = interp.griddata(self.Abar, self.U, Abar_p_new, method='cubic')
+                    m_p_new = interp.griddata(self.Abar, self.m, Abar_p_new, method='cubic')
+                    R_p_new = interp.griddata(self.Abar, self.R, Abar_p_new, method='cubic')
+                    if ( diff > 0): # move across one grid point
+                        interp_w = (self.Abar[idx_p_new] - self.Abar_p) / (Abar_p_new - self.Abar_p)
+                        # linear interpolation
+                        self.U_hm[idx_p_new] = U_p_new * interp_w + self.U_p * (1 - interp_w)
+                        self.m_hm[idx_p_new] = m_p_new * interp_w + self.m_p * (1 - interp_w)
+                        self.R_hm[idx_p_new] = R_p_new * interp_w + self.R_p * (1 - interp_w)
+                        self.xi_hm[idx_p_new] = self.xi + deltau * interp_w
+
+                    self.Abar_p = Abar_p_new
+                    self.U_p = U_p_new
+                    self.m_p = m_p_new
+                    self.R_p = R_p_new
+
+
+                kR1 = kR4.copy()
+                km1 = km4.copy()
+                kU1 = kU4.copy()
+                kA_p1 = kA_p4
+
+                step+=1
+                self.xi += deltau
+                if(idx_p_new == self.N-2): #going out of the boundary
+                    break
+
+            if( diff <=1):
+                # Adjust step size.
+                self.q = 0.8*np.min((max_err_R/E_R, max_err_m/E_m, max_err_U/E_U) )**(1/3)   # conservative optimal step factor
+                self.q = min(self.q,10)               # limit stepsize growth
+                deltau *= self.q
+
+            else:
+                # reducing the deltau since diff is too large
+                deltau /= 2
+
+            self.deltau_adap = deltau
+
+            if(step % 10 == 0):
+                if(find_exec_pos(self.R**2 * self.m * self.Abar**2 * np.exp(2 * (self.alpha-1) * self.xi)) > 0):
+                    print("Horizon is found, code has been terminated!")
+                    break
+
+        if step < n_steps:
+            print("Photon has gone out of the outter boundary or horizon is identified!")
+
+    def cfl_deltau(self,R, m, U):
+        a = np.exp(self.alpha * self.xi)
+        H = np.exp(-self.xi) / self.RH
+
+        g = self.gamma(R, m, U, self.xi)
+        r = self.rho(R, m)
+        p = self.P(r)
+
+        Pprime = self.Pprime(R, m)
+
+        ep = np.exp(self.psi(r, p, Pprime))
+        el =  (dfdA(a * self.A * self.R, self.deltaL, self.Abar, 1) / self.RH) /(a * H * self.RH * g)
+
+        return (np.log(1 + el / ep / np.exp(self.xi)
+                       * self.alpha * np.concatenate( ([1e10],(self.Abar[1:] - self.Abar[0:-1])) ) / np.sqrt(self.w))).min()
---
+++ ms_hm/cython/utils.pyx
@@ -0,0 +1,262 @@
+import numpy as np
+import cython
+cimport numpy as np
+
+cimport cython
+ctypedef np.double_t DTYPE_t
+
+from libc.math cimport exp
+from libc.math cimport sqrt
+
+from cython.parallel import prange
+
+# first derivative
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef dfdA(np.ndarray arr, double deltaL, double [::1] A, double bd = 0, int exec_pos = -1):
+    cdef int size = arr.shape[0]
+    cdef double [:] f = arr
+    cdef double [:] res =  np.zeros(size, dtype=np.double)
+
+    cdef int i
+    for i in prange(exec_pos+2, size - 1,nogil=True):
+        res[i] = (f[i+1] - f[i-1]) / (A[i+1] - A[i-1])
+    if(exec_pos > -1):
+        res[exec_pos+1] = (f[exec_pos+2] - f[exec_pos+1]) / (A[exec_pos+2]-A[exec_pos+1])
+    elif(bd < 1e99): # if bd value is valid, set 0th component to bd
+        res[0] = bd
+    else: # invalid bd value, so use one-side derivative
+        res[0] = (f[1] - f[0]) / (A[1] - A[0])
+        #res[0] = (-11 * f[0] / 6 + 3 * f[1] - 3 * f[2] / 2 + f[3] / 3) / (A[1] - A[0])
+    res[size - 1] = (f[-1] - f[-2]) / (A[size - 1] - A[size-2])
+    return np.asarray(res)
+
+# first derivative
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef stg_dfdA(np.ndarray arr, double [::1] A):
+    cdef int size = arr.shape[0]
+    cdef double [:] f = arr
+    cdef double [:] res =  np.zeros(size-1, dtype=np.double)
+
+    cdef int i
+    for i in prange(0, size - 1,nogil=True):
+        res[i] = (f[i+1] - f[i]) / (A[i+1] - A[i])
+
+    return np.asarray(res)
+
+# first derivative
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef WENO_dfdA(np.ndarray arr, double deltaL, double [::1] A, double bd=0):
+    cdef int size = arr.shape[0]
+    cdef double [:] f = arr
+    cdef double [:] st_f =  np.zeros(size, dtype=np.double) # i+1/2 field
+    cdef double [:] res =  np.zeros(size, dtype=np.double)
+
+    cdef int i
+    cdef double b1, b2, b3, w1, w2, w3, ws, st_f1, st_f2, st_f3
+    for i in prange(2, size - 2, nogil=True):
+        b1 = (4 * f[i-2]**2 - 19 * f[i-2] * f[i-1] + 25 * f[i-1]**2 + 11 * f[i-2] * f[i] - 31 * f[i-1] * f[i] + 10 * f[i]**2) / 3
+        b2 = (4 * f[i-1]**2 - 13 * f[i-1] * f[i] + 13 * f[i]**2 + 5 * f[i-1] * f[i+1] - 13 * f[i] * f[i+1] + 4 * f[i+1]**2) / 3
+        b3 = (10 * f[i]**2 - 31 * f[i] * f[i+1] + 25 * f[i+1]**2 + 11 *f[i] * f[i+2] - 19 * f[i+1] * f[i+2] + 4 * f[i+2]**2) / 3
+        w1 = 1/10/(1e-6 + b1)**2
+        w2 = 6/10/(1e-6 + b2)**2
+        w3 = 3/10/(1e-6 + b3)**2
+        ws = w1 + w2 + w3
+        w1 = w1 / ws
+        w2 = w2 / ws
+        w3 = w3 / ws
+        st_f1 = 3 * f[i-2] / 8 - 5 * f[i-1] / 4 + 15 * f[i] / 8
+        st_f2 = -f[i-1] / 8 + 3 * f[i] / 4 + 3 * f[i+1] / 8
+        st_f3 = 3 * f[i] / 8 + 3 * f[i+1] /4 - f[i+2] / 8
+        st_f[i] = st_f1 * w1 + st_f2 * w2 + st_f3 * w3
+
+
+    for i in prange(3, size - 2, nogil=True):
+        res[i] = (st_f[i] - st_f[i-1]) / ((A[i+1] - A[i-1]) / 2)
+
+    res[2] = (f[3] - f[1]) / (A[3] - A[1])
+    res[1] = (f[2] - f[0]) / (A[2] - A[0])
+    res[0] = (f[1] - f[0]) / (A[1] - A[0])
+
+    res[size - 1] = (f[size-1] - f[size-2]) / (A[size-1] - A[size-2])
+    res[size-2] = (f[size-1] - f[size-3]) / (A[size-1] - A[size-3])
+
+    return np.asarray(res)
+
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef WENO_to_stg(np.ndarray arr):
+    cdef int size = arr.shape[0]
+    cdef double [:] f = arr
+    cdef double [:] st_f =  np.zeros(size-1, dtype=np.double) # i+1/2 field
+
+    cdef int i
+    cdef double b1, b2, b3, w1, w2, w3, ws, st_f1, st_f2, st_f3
+    for i in prange(2, size - 2,nogil=True):
+        b1 = (4 * f[i-2]**2 - 19 * f[i-2] * f[i-1] + 25 * f[i-1]**2 + 11 * f[i-2] * f[i] - 31 * f[i-1] * f[i] + 10 * f[i]**2) / 3
+        b2 = (4 * f[i-1]**2 - 13 * f[i-1] * f[i] + 13 * f[i]**2 + 5 * f[i-1] * f[i+1] - 13 * f[i] * f[i+1] + 4 * f[i+1]**2) / 3
+        b3 = (10 * f[i]**2 - 31 * f[i] * f[i+1] + 25 * f[i+1]**2 + 11 *f[i] * f[i+2] - 19 * f[i+1] * f[i+2] + 4 * f[i+2]**2) / 3
+        w1 = 1/10/(1e-6 + b1)**2
+        w2 = 6/10/(1e-6 + b2)**2
+        w3 = 3/10/(1e-6 + b3)**2
+        ws = w1 + w2 + w3
+        w1 = w1 / ws
+        w2 = w2 / ws
+        w3 = w3 / ws
+        st_f1 = 3 * f[i-2] / 8 - 5 * f[i-1] / 4 + 15 * f[i] / 8
+        st_f2 = -f[i-1] / 8 + 3 * f[i] / 4 + 3 * f[i+1] / 8
+        st_f3 = 3 * f[i] / 8 + 3 * f[i+1] /4 - f[i+2] / 8
+        st_f[i] = st_f1 * w1 + st_f2 * w2 + st_f3 * w3
+
+    st_f[1] = (f[2] + f[1]) / 2
+    st_f[0] = (f[1] + f[0]) / 2
+
+    st_f[size-2] = (f[size-1] + f[size-2]) / 2
+
+    return np.asarray(st_f)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef inv_derv_phi(np.ndarray rho_in, double [:] P, double off_set=0, int exec_pos = -1):
+    cdef int size = rho_in.shape[0]
+    cdef double [:] rho = rho_in
+    cdef double [:] res =  np.zeros(size, dtype=np.double)
+    cdef int i
+    res[size - 1] = off_set
+    res[size - 2] =  (P[size-1] - P[size-2]) / (P[size-2] + rho[size-2]) + res[size-1]
+    for i in range(size-3, -1, -1):
+        res[i] = (P[i+2] - P[i]) / (P[i+1] + rho[i+1]) + res[i+2]
+    return np.asarray(res)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+# for exp(\psi) in Eq. 165e
+cpdef derv_psi( np.ndarray xi, np.ndarray c, double off_set=0):
+    cdef int size = xi.shape[0]
+    cdef double [:] res =  np.zeros(size, dtype=np.double)
+    cdef int i
+    res[0] = off_set
+    res[1] = - (xi[2] - xi[1]) * c[0] + res[0]
+
+    for i in range(2, size):
+        res[i] = - (xi[i] - xi[i-2]) * c[i - 1] + res[i-2]
+    return np.asarray(res)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+# for exp(\psi) in Eq. 165e
+cpdef inv_derv_psi( np.ndarray xi_in, double [::1] c, double off_set=0):
+    cdef int size = xi_in.shape[0]
+    cdef double [:] xi = xi_in
+    cdef double [:] res =  np.zeros(size, dtype=np.double)
+    cdef int i
+    res[size - 1] = off_set
+    res[size - 2] =  (xi[size-1] - xi[size-2]) * c[size-2] + res[size-1]
+    #for i in range(size-3, -1, -1):
+    #    res[i] = (xi[i+1] - xi[i]) * c[i] + res[i+1]
+    for i in range(size-3, -1, -1):
+        res[i] = (xi[i+2] - xi[i]) * c[i+1] + res[i+2]
+    return np.asarray(res)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef find_exec_pos(np.ndarray arr):
+    cdef int size = arr.shape[0]
+    cdef double [:] f = arr
+
+    cdef int i
+    for i in range(size - 1, 1,-1):
+        if(arr[i] < 1 and arr[i-1] > 1): # found a horizon
+            return i-1
+    return -1
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef ms_rho_term(np.ndarray R_in, double [:] m, double [:] A):
+    cdef int size = R_in.shape[0]
+    cdef double [:] res =  np.zeros(size, dtype=np.double)
+    cdef double [:] R = R_in
+    cdef int i
+    for i in prange(1, size - 1,nogil=True):
+        res[i] = A[i] * R[i] * (m[i+1] - m[i-1]) \
+                / (3 * (A[i+1] * R[i+1] - A[i-1] * R[i-1]))
+    i = 0
+    res[i] = A[i] * R[i] * (m[i+1] - m[i]) \
+                / (3 * (A[i+1] * R[i+1] - A[i] * R[i]))
+    i = size - 1
+    res[i] = A[i] * R[i] * (m[i] - m[i-1]) \
+                / (3 * (A[i] * R[i] - A[i-1] * R[i-1]))
+    return np.asarray(res)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef ms_rho_term_stg(np.ndarray R_in, double [:] m, double [:] A,
+                     double [:] R_stg, double [:] A_stg):
+    cdef int size = R_in.shape[0]
+    cdef double [:] res =  np.zeros(size-1, dtype=np.double)
+    cdef double [:] R = R_in
+    cdef int i
+    for i in prange(0, size - 1,nogil=True):
+        res[i] = A_stg[i] * R_stg[i] * (m[i+1] - m[i]) \
+                / (3 * (A[i+1] * R[i+1] - A[i] * R[i]))
+    return np.asarray(res)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef hm_rho_term(np.ndarray R_in, double [:] m, double [:] A, double [:] xi,
+               double a):
+    cdef int size = R_in.shape[0]
+    cdef double [:] res =  np.zeros(size, dtype=np.double)
+    cdef double [:] R = R_in
+    cdef int i
+    for i in prange(1, size - 1,nogil=True):
+        res[i] = (m[i+1] - m[i-1] - 2 * m[i] * (xi[i+1] - xi[i-1])) \
+                / (a * A[i] * R[i] * (xi[i+1] - xi[i-1]) + (A[i+1] * R[i+1] - A[i-1] * R[i-1]))
+
+    i = 0
+    res[0] = (m[i+1] - m[i] - 2 * m[i] * (xi[i+1] - xi[i])) \
+                / (a * A[i] * R[i] * (xi[i+1] - xi[i]) + (A[i+1] * R[i+1] - A[i] * R[i]))
+    i = size - 1
+    res[size-1] = (m[size-1] - m[i-1] - 2 * m[i] * (xi[size-1] - xi[i-1])) \
+                / (a * A[i] * R[i] * (xi[size-1] - xi[i-1]) + (A[size-1] * R[size-1] - A[i-1] * R[i-1]))
+    return np.asarray(res)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef rho_term_stg(np.ndarray R_in, double [:] m, double [:] A, double [:] xi,
+               double [:] R_stg, double [:] m_stg, double [:] A_stg, double a):
+    cdef int size = R_in.shape[0]
+    cdef double [:] res =  np.zeros(size-1, dtype=np.double)
+    cdef double [:] R = R_in
+    cdef int i
+    for i in prange(0, size - 1,nogil=True):
+        res[i] = (m[i+1] - m[i] - 2 * m_stg[i] * (xi[i+1] - xi[i])) \
+                / (a * A_stg[i] * R_stg[i] * (xi[i+1] - xi[i]) + (A[i+1] * R[i+1] - A[i] * R[i]))
+
+    return np.asarray(res)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef ephi_term(np.ndarray R_in, double [:] U, double [:] A, double [:] xi, double [:] g,
+               double a):
+    cdef int size = R_in.shape[0]
+    cdef double [:] res =  np.zeros(size, dtype=np.double)
+    cdef double [:] R = R_in
+    cdef int i
+    for i in prange(1, size - 1,nogil=True):
+        res[i] = (a * (xi[i+1] - xi[i-1]) * A[i] * R[i] + (A[i+1] * R[i+1] - A[i-1] * R[i-1])) /\
+                (a * (xi[i+1] - xi[i-1]) * (g[i] + A[i] * R[i] * U[i]))
+
+    i = 0
+    res[0] = (a * (xi[i+1] - xi[i]) * A[i] * R[i] + (A[i+1] * R[i+1] - A[i] * R[i])) /\
+                (a * (xi[i+1] - xi[i]) * (g[i] + A[i] * R[i] * U[i]))
+    i = size - 1
+    res[size-1] = (a * (xi[size-1] - xi[i-1]) * A[i] * R[i] + (A[size-1] * R[size-1] - A[i-1] * R[i-1])) /\
+                (a * (xi[size-1] - xi[i-1]) * (g[i] + A[i] * R[i] * U[i]))
+    return np.asarray(res)
+
+@cython.boundscheck(False)  # Deactivate bounds checking
+cpdef rho_prime(np.ndarray R_in, double [:] m, double [:] A, double [:] xi,
+               double [:] Rp, double [:] mp, double [:] xip,
+               double [:] Rpp, double [:] mpp, double [:] xipp, double a):
+    cdef int size = R_in.shape[0]
+    cdef double [:] res =  np.zeros(size, dtype=np.double)
+    cdef double [:] R = R_in
+    cdef int i
+    for i in prange(0, size-1 ,nogil=True):
+        res[i] = ((R[i] + A[i] * Rp[i] + A[i] * a * R[i] * xip[i]  )
+                  * (-2 * mp[i] * xip[i] + mpp[i] - 2 * m[i] * xipp[i])
+                 - (mp[i] - 2 * m[i] * xip[i] ) * (Rp[i] * (2 + A[i] * a * xip[i])
+                        + A[i] * Rpp[i] + a * R[i] * (xip[i] + A[i] * xipp[i])) ) \
+                / (R[i] + A[i] * Rp[i] + A[i] * a * R[i] * xip[i])**2
+    return np.asarray(res)
