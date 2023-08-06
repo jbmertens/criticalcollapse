c01b34af8d945d489a567c9d366ac1842a49a08c
James Mertens <jmertens@wustl.edu>
2023-08-06 14:14:55 -0500

    changes to c code for more interaction

--- cpp/collapse.cc
+++ cpp/collapse.cc
@@ -7,6 +7,12 @@
 #include <getopt.h>
 #include <string>
 
+// #include <boost/math/interpolators/makima.hpp>
+// using boost::math::interpolators::makima;
+// #include <utility>
+#include <vector>
+#include "spline.h"
+
 typedef float real_t;
 
 int NN = 3200;
@@ -18,57 +24,42 @@ int NN = 3200;
 
 #define OUTPUT true
 
-#define W0 (1.0/3.0)  //( P(RHO0)/RHO0 )
-#define ALPHA (2.0/3.0/(1 + W0))
+#define ALPHA (2.0/3.0/(1 + fixw))
 #define T0 (ALPHA * std::sqrt(3 / (8*PI*rho0)))
 #define RH (T0 / ALPHA)
 
 #define TOL (1.0e-7) // integration tolerance
 
 
-#define ALLOC(arr_n) \
+#define ALLOC_N(arr_n, ARR_SIZE) \
     real_t *arr_n; \
-    arr_n = (real_t *) malloc(NN * ((long long) sizeof(real_t)));
+    arr_n = (real_t *) malloc( (ARR_SIZE) * ((long long) sizeof(real_t)));
 
-#define AUX_ALLOC(aux_n) \
-    aux_t aux_n; \
-    aux_n.dRtildedA = (real_t *) malloc(NN * ((long long) sizeof(real_t))); \
-    aux_n.dmtildedA = (real_t *) malloc(NN * ((long long) sizeof(real_t))); \
-    aux_n.gamma = (real_t *) malloc(NN * ((long long) sizeof(real_t))); \
-    aux_n.rho = (real_t *) malloc(NN * ((long long) sizeof(real_t))); \
-    aux_n.P = (real_t *) malloc(NN * ((long long) sizeof(real_t))); \
-    aux_n.dPdA = (real_t *) malloc(NN * ((long long) sizeof(real_t))); \
-    aux_n.phi = (real_t *) malloc(NN * ((long long) sizeof(real_t))); \
-    aux_n.m2oR = (real_t *) malloc(NN * ((long long) sizeof(real_t))); \
-    aux_n.Q = (real_t *) malloc(NN * ((long long) sizeof(real_t)));
+#define ALLOC(arr_n) \
+    ALLOC_N(arr_n, NN)
 
-#define AUX_FREE(aux_n) \
-    free(aux_n.dRtildedA);\
-    free(aux_n.dmtildedA);\
-    free(aux_n.gamma);\
-    free(aux_n.rho);\
-    free(aux_n.P);\
-    free(aux_n.dPdA);\
-    free(aux_n.phi);\
-    free(aux_n.m2oR);\
-    free(aux_n.Q);
+std::vector<real_t> logrho{-23.0258509, 1.25944027,  5.86743592, 10.47443832, 12.80727797, 16.24077033,
+   20.17741006, 21.87146739, 22.54685293, 24.86073802, 25.88516263, 30.81629677,
+   40.14966858, 42.94194384, 45.77729053, 49.56660234, 53.73838727, 92.1034037};
+   
+std::vector<real_t> logP{-24.1244632, 0.15168708,  4.7676633 ,  9.37390511, 11.68836246, 15.05383501,
+   18.98355404, 20.54248356, 21.11727849, 23.49996688, 24.58907975, 29.64524541,
+   39.04613022, 41.81963086, 44.6428769 , 48.43776105, 52.63885477, 91.0047914};
 
-typedef struct {
-    real_t *dRtildedA;
-    real_t *dmtildedA;
+tk::spline loglog_Pinterp(logrho, logP);
 
-    real_t *gamma;
-    real_t *rho;
 
-    real_t *P;
-    real_t *dPdA;
+real_t min_rho=1e0;
+real_t max_rho=1e23;
+real_t mu=2;
 
-    real_t *phi;
+bool use_fixw = false;
+real_t fixw = 1.0/3.0;
 
-    real_t *m2oR;
-    real_t *Q;
-} aux_t;
-
+real_t H(real_t rho, real_t rho0)
+{
+    return ( 1 + std::tanh( (std::log(rho) - std::log(rho0))/mu ) )/2;
+}
 
 void dfdA(real_t *f, real_t *A, real_t *dfdA)
 {
@@ -86,14 +77,43 @@ void dfdA(real_t *f, real_t *A, real_t *dfdA)
     }
 }
 
-real_t Prho(real_t rho)
+extern "C"
+real_t P_of_rho(real_t rho)
 {
-    return W0*rho;
+    if(rho < 1.0e-10)
+        rho = 1.0e-10;
+
+    if(use_fixw)
+        return fixw*rho;
+
+    real_t logrho = std::log(rho);
+    if(logrho < -23.0 or logrho > 93.0)
+        return rho/3.0;
+
+    real_t P_in = std::exp(loglog_Pinterp(logrho));
+    real_t P_out = rho/3.0;
+    real_t P = P_out + H(rho, min_rho)*H(max_rho, rho)*(P_in - P_out);
+
+    return P;
 }
 
+extern "C"
 real_t dPdrho(real_t rho)
 {
-    return W0;
+    if(rho < 1.0e-10)
+        rho = 1.0e-10;
+
+    if(use_fixw)
+        return fixw;
+
+    real_t P = P_of_rho(rho);
+    real_t logrho = std::log(rho);
+
+    real_t dPdrho_in = P/rho*loglog_Pinterp.deriv(1, logrho);
+    real_t dPdrho_out = 1.0/3.0;
+    real_t dPdrho = dPdrho_out + H(rho, min_rho)*H(max_rho, rho)*(dPdrho_in - dPdrho_out);
+
+    return dPdrho;
 }
 
 void dPdAstg(real_t *R, real_t *m, real_t *A, real_t *dPdA)
@@ -112,7 +132,7 @@ void dPdAstg(real_t *R, real_t *m, real_t *A, real_t *dPdA)
         real_t m_stg = (m[i]+m[i+1])/2;
         real_t rho_stg = m_stg + A_stg*R_stg*(m[i+1] - m[i])
                             / 3 / (A[i+1]*R[i+1] - A[i]*R[i]);
-        P_stg = Prho(rho_stg);
+        P_stg = P_of_rho(rho_stg);
 
         dPdA[i] = (P_stg - P_stg_prev)/(A_stg - A_stg_prev);
     }
@@ -122,17 +142,44 @@ void dPdAstg(real_t *R, real_t *m, real_t *A, real_t *dPdA)
 }
 
 
-void aux_pop(real_t *Rtilde, real_t *mtilde, real_t *Utilde,
-    real_t *Abar, real_t xi, aux_t aux)
+void agg_pop(
+    real_t *Abar_in, real_t *Rtilde_in, real_t *mtilde_in, real_t *Utilde_in,
+    real_t xi,
+    real_t * agg)
 {
     int i=0;
     real_t e2ax = std::exp(2*(1 - ALPHA)*xi);
 
-    dfdA(Rtilde, Abar, aux.dRtildedA);
-    dfdA(mtilde, Abar, aux.dmtildedA);
+    // Populate all information; stored in "agg"regate array
+    real_t *Abar      = agg + 0;
+    real_t *Rtilde    = agg + 1*NN;
+    real_t *mtilde    = agg + 2*NN;
+    real_t *Utilde    = agg + 3*NN;
+    real_t *dRtildedA = agg + 4*NN;
+    real_t *dmtildedA = agg + 5*NN;
+    real_t *gamma     = agg + 6*NN;
+    real_t *rho       = agg + 7*NN;
+    real_t *P         = agg + 8*NN;
+    real_t *dPdA      = agg + 9*NN;
+    real_t *phi       = agg + 10*NN;
+    real_t *m2oR      = agg + 11*NN;
+    real_t *Q         = agg + 12*NN;
 
-    real_t *gamma = aux.gamma;
-    real_t *m2oR = aux.m2oR;
+    // A, R, m, U are read in
+#pragma omp parallel for default(shared) private(i)
+    for(i=0; i<NN; i++)
+    {
+        Abar[i] = Abar_in[i];
+        Rtilde[i] = Rtilde_in[i];
+        mtilde[i] = mtilde_in[i];
+        Utilde[i] = Utilde_in[i];
+    }
+
+    // Derivatives of R and m, used later
+    dfdA(Rtilde, Abar, dRtildedA);
+    dfdA(mtilde, Abar, dmtildedA);
+
+    // compute gamma and 2*m/R, used later
 #pragma omp parallel for default(shared) private(i)
     for(i=0; i<NN; i++)
     {
@@ -142,7 +189,7 @@ void aux_pop(real_t *Rtilde, real_t *mtilde, real_t *Utilde,
     }
 
 
-    real_t *rho = aux.rho;
+    // Compute density (rho)
 #pragma omp parallel for default(shared) private(i)
     for(i=1; i<NN-1; i++)
     {
@@ -154,15 +201,15 @@ void aux_pop(real_t *Rtilde, real_t *mtilde, real_t *Utilde,
                 / 3 / (Abar[NN-1]*Rtilde[NN-1] - Abar[NN-2]*Rtilde[NN-2]);
 
 
-    real_t *P = aux.P;
+    // Compute pressure
 #pragma omp parallel for default(shared) private(i)
     for(i=0; i<NN; i++)
-        P[i] = Prho(rho[i]);
+        P[i] = P_of_rho(rho[i]);
 
+    // Compute (artificial) viscosity
     real_t kappa = 2.0;
     if(kappa > 0)
     {
-        real_t *Q = aux.Q;
 #pragma omp parallel for default(shared) private(i)
         for(i=1; i<NN-1; i++)
         {
@@ -181,29 +228,29 @@ void aux_pop(real_t *Rtilde, real_t *mtilde, real_t *Utilde,
         for(i=2; i<NN-2; i++)
             Q[i] = (Q[i-2]+2*Q[i-1]+3*Q[i]+2*Q[i+1]+Q[i+2])/9.0;
 
-
 #pragma omp parallel for default(shared) private(i)
         for(i=0; i<NN; i++)
             P[i] += Q[i];
     }
 
-    dPdAstg(Rtilde, mtilde, Abar, aux.dPdA);
+    // Compute derivative of pressure, using staggered grid
+    dPdAstg(Rtilde, mtilde, Abar, dPdA);
 
-    real_t *phi = aux.phi;
+    // Perform integration to compute metric (phi)
     phi[NN-1] = -1.5*ALPHA*dPdrho(rho[NN-1])*std::log(rho[NN-1]);
     phi[NN-2] = phi[NN-1] + (P[NN-1] - P[NN-2])/(P[NN-2] + rho[NN-2]);
     for(i=NN-3; i>=0; i--)
         phi[i] = (P[i+2] - P[i])/(P[i+1] + rho[i+1]) + phi[i+2];
-
 }
 
+
+// Compute Runge-Kutta 'k' coefficients
 void k_calc(real_t *Rtilde, real_t *kR_p, real_t *kR_f,
     real_t *mtilde, real_t *km_p, real_t *km_f,
     real_t *Utilde, real_t *kU_p, real_t *kU_f,
-    real_t *Abar, aux_t aux, real_t xi, real_t deltaxi,
+    real_t *Abar, real_t *agg, real_t xi, real_t deltaxi,
     real_t kcoeff)
 {
-    // Compute Runge-Kutta "k" coefficients
     int i=0;
 
     // Store intermediate values in _f register
@@ -216,8 +263,17 @@ void k_calc(real_t *Rtilde, real_t *kR_p, real_t *kR_f,
     }
     xi = xi + deltaxi*kcoeff;
 
-    // Populate auxiliary field values
-    aux_pop(kR_f, km_f, kU_f, Abar, xi, aux);
+    // Populate "agg" with computed values,
+    // extract them in relevant arrays
+    agg_pop(Abar, kR_f, km_f, kU_f, xi, agg);
+    real_t *dRtildedA = agg + 4*NN;
+    real_t *dmtildedA = agg + 5*NN;
+    real_t *gamma     = agg + 6*NN;
+    real_t *rho       = agg + 7*NN;
+    real_t *P         = agg + 8*NN;
+    real_t *dPdA      = agg + 9*NN;
+    real_t *phi       = agg + 10*NN;
+    real_t *Q         = agg + 12*NN;
 
     // fill _f registers with final values
 #pragma omp parallel for default(shared) private(i)
@@ -226,79 +282,40 @@ void k_calc(real_t *Rtilde, real_t *kR_p, real_t *kR_f,
         real_t R = kR_f[i];
         real_t m = km_f[i];
         real_t U = kU_f[i];
-        real_t ep = std::exp(aux.phi[i]);
-        real_t AR_prime = R + Abar[i]*aux.dRtildedA[i];
+        real_t ep = std::exp(phi[i]);
+        real_t AR_prime = R + Abar[i]*dRtildedA[i];
 
         real_t dPdAoA = 0;
         if(i == 0)
-            dPdAoA = 5.0/3.0*dPdrho(aux.rho[0])
+            dPdAoA = 5.0/3.0*dPdrho(rho[0])
                 *(km_f[1] + km_f[1] - 2*km_f[0]) / Abar[1] / Abar[1];
         else
-            dPdAoA = aux.dPdA[i] / Abar[i];
+            dPdAoA = dPdA[i] / Abar[i];
 
         kR_f[i] = ALPHA*R*(U*ep - 1);
-        km_f[i] = 2*m - 3*ALPHA*U*ep*(aux.P[i] + m);
+        km_f[i] = 2*m - 3*ALPHA*U*ep*(P[i] + m);
         kU_f[i] = U - ALPHA*ep*(
-            aux.gamma[i]*aux.gamma[i]*dPdAoA / (R*AR_prime*(aux.rho[i] + aux.P[i]))
-            + (2*U*U + m + 3*aux.P[i])/2
+            gamma[i]*gamma[i]*dPdAoA / (R*AR_prime*(rho[i] + P[i]))
+            + (2*U*U + m + 3*P[i])/2
         );
     }
 
 }
 
-void gather_output(real_t *Rtilde, real_t *mtilde, real_t *Utilde,
-    real_t *Abar, real_t xi, real_t *output)
+// Write output to file
+void write_output(real_t *Abar, real_t *Rtilde, real_t *mtilde, real_t *Utilde,
+    real_t *agg,
+    real_t xi, std::ofstream & output)
 {
-    AUX_ALLOC(aux)
-    aux_pop(Rtilde, mtilde, Utilde, Abar, xi, aux);
-
-    int i=0;
-#pragma omp parallel for default(shared) private(i)
-    for(i=0; i<NN; i++)
-    {
-        output[ 0*NN + i] = Rtilde[i];
-        output[ 1*NN + i] = mtilde[i];
-        output[ 2*NN + i] = Utilde[i];
-        output[ 3*NN + i] = Abar[i];
-        output[ 4*NN + i] = aux.dRtildedA[i];
-        output[ 5*NN + i] = aux.dmtildedA[i];
-        output[ 6*NN + i] = aux.gamma[i];
-        output[ 7*NN + i] = aux.rho[i];
-        output[ 8*NN + i] = aux.P[i];
-        output[ 9*NN + i] = aux.dPdA[i];
-        output[10*NN + i] = aux.phi[i];
-        output[11*NN + i] = aux.m2oR[i];
-        output[12*NN + i] = aux.Q[i];
-    }
-
-    AUX_FREE(aux)
+    agg_pop(Abar, Rtilde, mtilde, Utilde, xi, agg);
+    output.write((char *) agg, 13*NN*sizeof(real_t));
 }
 
-void write_output(real_t *Rtilde, real_t *mtilde, real_t *Utilde,
-    real_t *Abar, real_t xi, std::ofstream & output)
-{
-    AUX_ALLOC(aux)
-    aux_pop(Rtilde, mtilde, Utilde, Abar, xi, aux);
-
-    output.write((char *) Rtilde, NN*sizeof(real_t));
-    output.write((char *) mtilde, NN*sizeof(real_t));
-    output.write((char *) Utilde, NN*sizeof(real_t));
-    output.write((char *) Abar, NN*sizeof(real_t));
-    output.write((char *) aux.dRtildedA, NN*sizeof(real_t));
-    output.write((char *) aux.dmtildedA, NN*sizeof(real_t));
-    output.write((char *) aux.gamma, NN*sizeof(real_t));
-    output.write((char *) aux.rho, NN*sizeof(real_t));
-    output.write((char *) aux.P, NN*sizeof(real_t));
-    output.write((char *) aux.dPdA, NN*sizeof(real_t));
-    output.write((char *) aux.phi, NN*sizeof(real_t));
-    output.write((char *) aux.m2oR, NN*sizeof(real_t));
-    output.write((char *) aux.Q, NN*sizeof(real_t));
-
-    AUX_FREE(aux)
-}
 
 extern "C"
-real_t * run_sim(int n, int steps, real_t amp, real_t rho0, int output_interval)
+int run_sim(int n, int steps, real_t amp, real_t rho0, int output_interval,
+    real_t *Abar, real_t *Rtilde, real_t *mtilde, real_t *Utilde,
+    real_t *agg, real_t &xi)
 {
     NN = n;
     
@@ -311,29 +328,14 @@ real_t * run_sim(int n, int steps, real_t amp, real_t rho0, int output_interval)
     std::cout << "\n" << std::flush;
 
     ALLOC(zeros)
-    ALLOC(Abar) ALLOC(Rtilde) ALLOC(mtilde) ALLOC(Utilde)
     ALLOC(kR1) ALLOC(kR2) ALLOC(kR3) ALLOC(kR4) ALLOC(Rnew)
     ALLOC(km1) ALLOC(km2) ALLOC(km3) ALLOC(km4) ALLOC(mnew)
     ALLOC(kU1) ALLOC(kU2) ALLOC(kU3) ALLOC(kU4) ALLOC(Unew)
 
-    AUX_ALLOC(aux)
-
-    real_t xi = 0.0;
-
-    // initial field values
     int i=0;
 #pragma omp parallel for default(shared) private(i)
     for(i=0; i<NN; i++)
-    {
         zeros[i] = 0.0;
-        Abar[i] = i*L/NN;
-        real_t delta0 = amp * std::exp(-Abar[i]*Abar[i] / 2 / OD_SIZE / OD_SIZE);
-        real_t delta0P = amp * delta0 * 2 * (-1 / 2 / OD_SIZE / OD_SIZE ) * Abar[i];
-        mtilde[i] = 1 + delta0;
-        Utilde[i] = 1 - ALPHA * delta0 / 2;
-        Rtilde[i] = 1 - ALPHA / 2 * (delta0 + W0 * Abar[i] * delta0P / (1 + 3*W0) );
-    }
-
 
     // main integration loop
     std::ofstream output;
@@ -349,13 +351,11 @@ real_t * run_sim(int n, int steps, real_t amp, real_t rho0, int output_interval)
             //     self.timer.stop("adap_run_steps")
             //     return -2
 
-            // self.timer.start("delta_calc")
             // if(self.delta == -1):
             //     r = self.rho(self.R, self.m)
             //     pos = zero_crossing(self.Abar, (self.Abar - 1/np.exp((self.alpha-1) * self.xi) / self.R))
             //     if(pos > 0 and np.interp(pos, self.Abar, r) < 1):
             //         self.delta = np.interp(pos, self.Abar, self.m) - 1
-            // self.timer.stop("delta_calc")
 
             // self.m2oR_prev = self.m2oR
             // self.m2oR = self.R**2 * self.m * self.Abar**2 * np.exp(2 * (self.alpha-1) * self.xi)
@@ -368,18 +368,18 @@ real_t * run_sim(int n, int steps, real_t amp, real_t rho0, int output_interval)
             #pragma omp critical
             if(s%output_interval==0 or s==steps)
                 if(output_interval > 0)
-                    write_output(Rtilde, mtilde, Utilde, Abar, xi, output);
+                    write_output(Abar, Rtilde, mtilde, Utilde, agg, xi, output);
 
         // Integration details
 
         k_calc(Rtilde, zeros, kR1,  mtilde, zeros, km1,
-            Utilde, zeros, kU1,  Abar, aux, xi, deltaxi,  0.0);
+            Utilde, zeros, kU1,  Abar, agg, xi, deltaxi,  0.0);
 
         k_calc(Rtilde, kR1, kR2,  mtilde, km1, km2,
-            Utilde, kU1, kU2,  Abar, aux, xi, deltaxi,  0.5);
+            Utilde, kU1, kU2,  Abar, agg, xi, deltaxi,  0.5);
 
         k_calc(Rtilde, kR2, kR3,  mtilde, km2, km3,
-            Utilde, kU2, kU3,  Abar, aux, xi, deltaxi,  0.75);
+            Utilde, kU2, kU3,  Abar, agg, xi, deltaxi,  0.75);
 
 #pragma omp parallel for default(shared) private(i)
         for(i=0; i<NN; i++)
@@ -390,7 +390,7 @@ real_t * run_sim(int n, int steps, real_t amp, real_t rho0, int output_interval)
         }
 
         k_calc(Rnew, zeros, kR4,  mnew, zeros, km4,
-            Unew, zeros, kU4,  Abar, aux, xi, deltaxi,  1.0);
+            Unew, zeros, kU4,  Abar, agg, xi, deltaxi,  1.0);
 
         real_t E_R_max = 0, E_m_max = 0, E_U_max = 0,
                err_R_max = 0, err_m_max = 0, err_U_max = 0;
@@ -440,7 +440,7 @@ real_t * run_sim(int n, int steps, real_t amp, real_t rho0, int output_interval)
             std::cout << "            "<<E_R_max<<", "<<E_m_max<<", "<<E_U_max<<"\n";
             std::cout << "deltaxi was "<<deltaxi<<"\n";
             if(output_interval > 0)
-                write_output(Rtilde, mtilde, Utilde, Abar, xi, output);
+                write_output(Abar, Rtilde, mtilde, Utilde, agg, xi, output);
             break;
         }
 
@@ -451,18 +451,34 @@ real_t * run_sim(int n, int steps, real_t amp, real_t rho0, int output_interval)
 
     output.close();
 
-    real_t *all_out;
-    all_out = (real_t *) malloc(13 * NN * ((long long) sizeof(real_t)));
-    gather_output(Rtilde, mtilde, Utilde, Abar, xi, all_out);
+    agg_pop(Abar, Rtilde, mtilde, Utilde, xi, agg);
 
     free(zeros);
-    free(Abar); free(Rtilde); free(mtilde); free(Utilde);
     free(kR1); free(kR2); free(kR3); free(kR4); free(Rnew);
     free(km1); free(km2); free(km3); free(km4); free(mnew);
     free(kU1); free(kU2); free(kU3); free(kU4); free(Unew);
-    AUX_FREE(aux)
 
-    return all_out;
+    return 0;
+}
+
+extern "C"
+void ics(real_t *Abar, real_t *Rtilde, real_t *mtilde, real_t *Utilde,
+    real_t &xi, real_t amp, int n)
+{
+    NN = n;
+    xi = 0;
+
+    int i=0;
+#pragma omp parallel for default(shared) private(i)
+    for(i=0; i<NN; i++)
+    {
+        Abar[i] = i*L/NN;
+        real_t delta0 = amp * std::exp(-Abar[i]*Abar[i] / 2 / OD_SIZE / OD_SIZE);
+        real_t delta0P = amp * delta0 * 2 * (-1 / 2 / OD_SIZE / OD_SIZE ) * Abar[i];
+        Rtilde[i] = 1 - ALPHA / 2 * (delta0 + fixw * Abar[i] * delta0P / (1 + 3*fixw) );
+        mtilde[i] = 1 + delta0;
+        Utilde[i] = 1 - ALPHA * delta0 / 2;
+    }
 }
 
 
@@ -509,7 +525,7 @@ int main(int argc, char **argv)
                 break;
             case 'h':
             case '?':
-                fprintf(stdout, "\nusage: %s -N [gridpoints] -s [steps] -r [rho_0] -a [amp] \n", argv[0]);
+                fprintf(stdout, "\nusage: %s -N [gridpoints] -s [steps] -r [rho_0] -a [amp] -o [output_interval] \n", argv[0]);
                 fprintf(stdout, "All options are optional; if not specified, defaults will be used.\n");
                 return 0;
             default:
@@ -519,7 +535,24 @@ int main(int argc, char **argv)
     }
     if(!output_interval) output_interval = steps;
 
-    run_sim(NN, steps, amp, rho0, output_interval);
 
-    return 1;
+    ALLOC(Abar)
+    ALLOC(Rtilde)
+    ALLOC(mtilde)
+    ALLOC(Utilde)
+    ALLOC_N(agg, 13*NN)
+
+    real_t xi;
+    ics(Abar, Rtilde, mtilde, Utilde, xi, amp, NN);
+
+    run_sim(NN, steps, amp, rho0, output_interval,
+        Abar, Rtilde, mtilde, Utilde, agg, xi);
+
+    free(Abar);
+    free(Rtilde);
+    free(mtilde);
+    free(Utilde);
+    free(agg);
+
+    return 0;
 }
